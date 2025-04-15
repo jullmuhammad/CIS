d9562b2e939c2481d755d09ba331a16f46050276
jullmuhammad <muhamad.jul97@gmail.com>
2025-04-15 23:12:48 +0700

    Perubahan Master User

--- CIS/MasterUser.vb
+++ CIS/MasterUser.vb
@@ -1,3 +1,10 @@
 ﻿Public Class MasterUser
-
+    Dim SQL As String
+    Dim Proses As New ClassKoneksi
+    Dim tblUser, tblPasien As DataTable
+    Dim aksi As String
+    Dim shostname As String = System.Net.Dns.GetHostName
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Close()
+    End Sub
 End Class
